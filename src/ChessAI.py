a07e7bdebdab9bbadee475839274591d46ddbee4
ShubhamPhapale <94707673+ShubhamPhapale@users.noreply.github.com>
2024-06-14 00:19:43 +0530

    Negamax with alpha beta pruning

--- src/ChessAI.py
+++ src/ChessAI.py
@@ -3,7 +3,7 @@ import random
 piece_Score = {'K': 0, 'Q': 9, 'R': 5, 'B': 3, 'N': 3, 'P': 1}
 CHECKMATE = 1000
 STALEMATE = 0
-DEPTH = 3
+DEPTH = 4
 
 def find_Random_Move(valid_Moves):
     return valid_Moves[random.randint(0, len(valid_Moves) - 1)]
@@ -12,25 +12,30 @@ def find_Best_Move(gs, valid_Moves):
     global next_Move
     next_Move = None
     random.shuffle(valid_Moves)
-    find_Move_Nega_Max(gs, valid_Moves, DEPTH, 1 if gs.whiteToMove else -1)
+    find_Move_Nega_Max_Alpha_Beta(gs, valid_Moves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
     return next_Move
 
-def find_Move_Nega_Max(gs, valid_Moves, depth, turn):
+def find_Move_Nega_Max_Alpha_Beta(gs, valid_Moves, depth, alpha, beta, turn):
     global next_Move
     if depth == 0:
         return score_Board(gs) * turn
     
+    # implement move ordering here later
     max_Score = -CHECKMATE
     for move in valid_Moves:
         gs.make_Move(move)
         next_Moves = gs.get_Valid_Moves()
-        score = -find_Move_Nega_Max(gs, next_Moves, depth - 1, -turn)
+        score = -find_Move_Nega_Max_Alpha_Beta(gs, next_Moves, depth - 1, -beta, -alpha, -turn)
         if score > max_Score:
             max_Score = score
             if depth == DEPTH:
                 next_Move = move
                 print(next_Move.get_Chess_Notation(), max_Score)
         gs.undo_Move()
+        if max_Score > alpha:
+            alpha = max_Score
+        if alpha >= beta:
+            break
     return max_Score
     
 def score_Board(gs):
